f5c9d6f74d0725c2cad997654a0bc7c7d758c68e
gisselvanessa21 <gissel.cabascango@jakaysa.com>
2024-12-05 18:55:32 -0500

    fixed directory

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import {  Poppins} from 'next/font/google'
 import "./globals.css";
+import { Navbar } from "@/components";
 
 const poppins = Poppins({ 
   subsets: ['latin'],
@@ -22,6 +23,7 @@ export default function RootLayout({
       <body
         className={`${poppins.className} antialiased`}
       >
+        <Navbar/>
         <h2>hola de nuevo</h2>
         {children}
       </body>
---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,10 @@
+export const Navbar = () => {
+  return (
+    <nav className="flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded">
+        <span>CBS</span>
+        <div className="flex flex-1">
+        </div>
+        <a href="/register" className="mr-2">Register</a>
+    </nav>
+  )
+}
