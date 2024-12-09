bf88a41f0ad36aed18f6f20055f5cb754c248b37
gisselvanessa21 <gissel.cabascango@jakaysa.com>
2024-12-09 18:29:19 -0500

    feat/ Add user form inputs with icons

--- src/components/UserFormModal.tsx
+++ src/components/UserFormModal.tsx
@@ -1,6 +1,21 @@
 "use client";
-import { Button, Label, Modal, Radio } from "flowbite-react";
+import {
+  Alert,
+  Button,
+  Datepicker,
+  Label,
+  Modal,
+  Radio,
+  Select,
+  TextInput,
+} from "flowbite-react";
 import Image from "next/image";
+import { BsFillLaptopFill } from "react-icons/bs";
+import {  FaIdCard, FaUser } from "react-icons/fa";
+import { HiBuildingOffice2, HiInformationCircle } from "react-icons/hi2";
+import { IoMdMail } from "react-icons/io";
+import PasswordInput from "./passwordInput/PasswordInput";
+import { BiSolidCalendar } from "react-icons/bi";
 
 interface UserFormModalProps {
   openModal: boolean;
@@ -34,27 +49,287 @@ export const UserFormModal = ({
             </p>
             {/* Otros contenidos del modal */}
           </div>
-          <Label className="font-medium text-sm">Tipo de identificación*</Label>
 
-          <div className="flex flex-wrap -mx-2">
-            <div className="w-full md:w-1/3 lg:w-1/3 px-2">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+            {/* Tipo de identificación */}
+            <div>
+              <Label className="font-medium text-sm">
+                Tipo de identificación <span className="text-red-600">*</span>
+              </Label>
               <div className="grid grid-cols-2 gap-2">
                 <div className="flex items-center gap-2 b-gray-input p-2">
                   <Radio id="cedula" name="user-data" value="cedula" />
-                  <Label htmlFor="cedula">Cédula</Label>
+                  <Label className="text-xs font-normal" htmlFor="cedula">
+                    Cédula
+                  </Label>
                 </div>
                 <div className="flex items-center gap-2 b-gray-input p-2">
                   <Radio id="passport" name="user-data" value="Passport" />
-                  <Label htmlFor="passport">Pasaporte</Label>
+                  <Label className="text-xs font-normal" htmlFor="passport">
+                    Pasaporte
+                  </Label>
                 </div>
               </div>
             </div>
 
-            <div className="w-full md:w-1/3 lg:w-1/3 px-2">
-              <div className="p-4 bg-gray-200">Column 2</div>
+            {/* Identificación */}
+            <div>
+              <Label className="font-medium text-sm">
+                Identificación <span className="text-red-600">*</span>
+              </Label>
+              <div className="max-w-md">
+                <TextInput
+                  id="email4"
+                  type="text"
+                  icon={() => <FaIdCard className="w-5 h-5 icon-color" />}
+                  placeholder="100000000"
+                  color="white"
+                  required
+                />
+              </div>
             </div>
-            <div className="w-full md:w-1/3 lg:w-1/3 px-2">
-              <div className="p-4 bg-gray-300">Column 3</div>
+
+            {/* Primer nombre */}
+            <div>
+              <Label className="font-medium text-sm">
+                Primer nombre <span className="text-red-600">*</span>
+              </Label>
+              <div className="max-w-md">
+                <TextInput
+                  id="email4"
+                  type="text"
+                  icon={() => <FaUser className="w-5 h-5 icon-color" />}
+                  placeholder="Ingrese primer nombre"
+                  color="white"
+                  required
+                />
+              </div>
+            </div>
+
+            {/* Segundo nombre */}
+            <div>
+              <Label className="font-medium text-sm">
+                Segundo nombre <span className="text-red-600">*</span>
+              </Label>
+              <div className="max-w-md">
+                <TextInput
+                  id="email4"
+                  type="text"
+                  icon={() => <FaUser className="w-5 h-5 icon-color" />}
+                  placeholder="Ingrese segundo nombre"
+                  color="white"
+                  className="text-xs"
+                  required
+                />
+              </div>
+            </div>
+
+            {/* Apellido paterno */}
+            <div>
+              <Label className="font-medium text-sm">
+                Apellido paterno <span className="text-red-600">*</span>
+              </Label>
+              <div className="max-w-md">
+                <TextInput
+                  id="email4"
+                  type="text"
+                  icon={() => <FaUser className="w-5 h-5 icon-color" />}
+                  placeholder="Ingrese apellido paterno"
+                  color="white"
+                  required
+                />
+              </div>
+            </div>
+
+            {/* Apellido materno */}
+            <div>
+              <Label className="font-medium text-sm">
+                Apellido materno <span className="text-red-600">*</span>
+              </Label>
+              <div className="max-w-md">
+                <TextInput
+                  id="email4"
+                  type="text"
+                  icon={() => <FaUser className="w-5 h-5 icon-color" />}
+                  placeholder="Ingrese apellido materno"
+                  color="white"
+                  required
+                />
+              </div>
+            </div>
+            {/* Sucursal */}
+            <div>
+              <Label className="font-medium text-sm">
+                Sucursal <span className="text-red-600">*</span>
+              </Label>
+              <div className="max-w-md relative">
+                <HiBuildingOffice2 className="w-5 h-5 icon-color absolute z-10 select-icon-position" />
+                <Select
+                  id="sucursales"
+                  color="white"
+                  className="text-black"
+                  required
+                >
+                  <option disabled selected>
+                    Seleccione
+                  </option>
+                  <option>Sucursal 1</option>
+                  <option>Sucursal 2</option>
+                  <option>Sucursal 3</option>
+                  <option>Sucursal 4</option>
+                </Select>
+              </div>
+            </div>
+            {/* Departamento */}
+            <div>
+              <Label className="font-medium text-sm">
+                Departamento <span className="text-red-600">*</span>
+              </Label>
+              <div className="max-w-md relative">
+                <HiBuildingOffice2 className="w-5 h-5 icon-color absolute z-10 select-icon-position" />
+                <Select
+                  id="departamentos"
+                  color="white"
+                  className="text-black"
+                  required
+                >
+                  <option selected disabled>
+                    Seleccione
+                  </option>
+                  <option>Departamento 1</option>
+                  <option>Departamento 2</option>
+                  <option>Departamento 3</option>
+                  <option>Departamento 4</option>
+                </Select>
+              </div>
+            </div>
+            {/* Rol */}
+            <div>
+              <Label className="font-medium text-sm">
+                Rol <span className="text-red-600">*</span>
+              </Label>
+              <div className="max-w-md relative">
+                <HiBuildingOffice2 className="w-5 h-5 icon-color absolute z-10 select-icon-position" />
+                <Select
+                  id="departamentos"
+                  color="white"
+                  className="text-black"
+                  required
+                >
+                  <option selected disabled>
+                    Seleccione
+                  </option>
+                  <option>Rol 1</option>
+                  <option>Rol 2</option>
+                  <option>Rol 3</option>
+                  <option>Rol 4</option>
+                </Select>
+              </div>
+            </div>
+            {/* Correo Corporativo */}
+            <div>
+              <Label className="font-medium text-sm">
+                Correo corporativo <span className="text-red-600">*</span>
+              </Label>
+              <div className="max-w-md">
+                <TextInput
+                  id="email4"
+                  type="text"
+                  icon={() => <IoMdMail className="w-5 h-5 icon-color" />}
+                  placeholder="correo@servicio.com"
+                  color="white"
+                  required
+                />
+              </div>
+            </div>
+            {/* IP */}
+            <div>
+              <Label className="font-medium text-sm">
+                Ip <span className="text-red-600">*</span>
+              </Label>
+              <div className="max-w-md">
+                <TextInput
+                  id="email4"
+                  type="text"
+                  icon={() => (
+                    <BsFillLaptopFill className="w-5 h-5 icon-color" />
+                  )}
+                  placeholder="000.00.0.0"
+                  color="white"
+                  required
+                />
+              </div>
+            </div>
+            {/* Nombre de usuario */}
+            <div className="row-span-2">
+              <Label className="font-medium text-sm">
+                Nombre de usuario <span className="text-red-600">*</span>
+              </Label>
+              <div className="max-w-md">
+                <TextInput
+                  id="email4"
+                  type="text"
+                  icon={() => <FaUser className="w-5 h-5 icon-color" />}
+                  placeholder="Ingrese un usuario"
+                  color="white"
+                  required
+                />
+              </div>
+              <Alert
+                color="info"
+                icon={HiInformationCircle}
+                className="flex items-start"
+              >
+                Longitud de 5 a 8 caracteres. Incluye solo letras minúsculas. No
+                utilice espacios en blanco.
+              </Alert>
+            </div>
+            {/* Fecha de activación */}
+            <div>
+              <Label className="font-medium text-sm">
+                Fecha de activación <span className="text-red-600">*</span>
+              </Label>
+              <div className="max-w-md">
+                <div className="w-full">
+                  <Datepicker
+                    placeholder="Seleccione"
+                    className=""
+                    language="es-ES"
+                    icon={() => (
+                      <BiSolidCalendar className="w-5 h-5 icon-color" />
+                    )}
+                  />
+                </div>
+              </div>
+            </div>
+            {/* Fecha de vencimiento */}
+            <div>
+              <Label className="font-medium text-sm">
+                Fecha de vencimiento <span className="text-red-600">*</span>
+              </Label>
+              <div className="max-w-md">
+                <Datepicker
+                  className=""
+                  language="es-ES"
+                  icon={() => (
+                    <BiSolidCalendar className="w-5 h-5 icon-color" />
+                  )}
+                />
+              </div>
+            </div>
+            {/* Contraseña*/}
+            <div>
+              <Label className="font-medium text-sm">
+                Contraseña <span className="text-red-600">*</span>
+              </Label>
+              <PasswordInput />
+            </div>
+            {/* confirmar Contraseña */}
+            <div>
+              <Label className="font-medium text-sm">
+                Confirmar contraseña <span className="text-red-600">*</span>
+              </Label>
+              <PasswordInput />
             </div>
           </div>
         </Modal.Body>
---
+++ src/components/passwordInput/PasswordInput.tsx
@@ -0,0 +1,112 @@
+import React, { useState } from 'react';
+import { TextInput, Button, Popover } from 'flowbite-react';
+import { MdRemoveRedEye } from 'react-icons/md';
+import { IoIosLock, IoMdEyeOff } from 'react-icons/io';
+import "./styles.css";
+const PasswordInput = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  return (
+    <div>
+      
+      <Popover
+        trigger="hover"
+        content={
+          <div className="space-y-2 p-3">
+            <h3 className="font-semibold text-gray-900 dark:text-white">
+              Must have at least 6 characters
+            </h3>
+            <div className="grid grid-cols-4 gap-2">
+              <div className="h-1 bg-orange-300 dark:bg-orange-400"></div>
+              <div className="h-1 bg-orange-300 dark:bg-orange-400"></div>
+              <div className="h-1 bg-gray-200 dark:bg-gray-600"></div>
+              <div className="h-1 bg-gray-200 dark:bg-gray-600"></div>
+            </div>
+            <p>It’s better to have:</p>
+            <ul>
+              <li className="mb-1 flex items-center">
+                <svg
+                  className="me-2 h-3.5 w-3.5 text-green-400 dark:text-green-500"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 16 12"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M1 5.917 5.724 10.5 15 1.5"
+                  />
+                </svg>
+                Upper & lower case letters
+              </li>
+              <li className="mb-1 flex items-center">
+                <svg
+                  className="me-2.5 h-3 w-3 text-gray-300 dark:text-gray-400"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 14 14"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                  />
+                </svg>
+                A symbol (#$&)
+              </li>
+              <li className="flex items-center">
+                <svg
+                  className="me-2.5 h-3 w-3 text-gray-300 dark:text-gray-400"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 14 14"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                  />
+                </svg>
+                A longer password (min. 12 chars.)
+              </li>
+            </ul>
+          </div>
+        }
+      >
+        <div className="relative max-w-md">
+          <TextInput
+            id="password1"
+            type={showPassword ? 'text' : 'password'}
+            placeholder="Ingrese su contraseña"
+            required
+            icon={() => <IoIosLock  className="w-5 h-5 icon-color" />}
+
+          />
+          <Button
+            type="button"
+            color='white'
+            className="absolute inset-y-0 right-0 flex items-center bg-transparent hover:bg-transparent focus:bg-transparent active:bg-transparent"
+            onClick={togglePasswordVisibility}
+          >
+            {showPassword ? <IoMdEyeOff  className="w-6 h-6 text-orange" /> : <MdRemoveRedEye  className="w-6 h-6 text-orange" />}
+          </Button>
+        </div>
+      </Popover>
+    </div>
+  );
+};
+
+export default PasswordInput;
